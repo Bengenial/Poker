9fcddbff8e0612cf8e9e1626a4a56d51d5e76421
Bengenial <you@example.com>
2025-06-05 19:26:47 -0400

    Estructuras iniciales y leerCartas

--- poker.c
+++ poker.c
@@ -5,43 +5,106 @@
 #include "tdas/extra.h"
 #include <string.h>
 
+typedef struct{
+	int id;
+	char color[10]; //corazones, diamantes, tréboles, picas
+	char valor[3]; //2-10, J, Q, K, A
+}Carta;
+
+typedef struct{
+	Carta cartas[52]; // ver si es factible hacerlo con un array o una lista
+	int num_cartas;
+}Baraja;
+
+typedef struct{
+	char nombre[50];
+	int fichas;
+	List *mano;
+	char estado[10]; //jugando, retirado, ganador
+}Jugador;
+
+typedef struct{
+	Carta cartas[5];
+	int bote;
+}Mesa;
+
+typedef struct{
+	List *jugadores;
+	int numJugadores;
+	Jugador *jugadorActual;
+	Baraja baraja;
+	Mesa mesa;
+	int ronda;
+}Partida;
+
+void leerCartas(Baraja *baraja){
+	FILE *archivo = fopen("data/cartas_poker.csv", "r");
+	if (archivo == NULL){
+		perror(
+			"Error al abrir el archivo"); // Informa si el archivo no puede abrirse
+		return;
+	}
+	char **campos;
+	int i = 0;
+
+	campos = leer_linea_csv(archivo, ','); // Lee los encabezados del CSV
+
+	while ((campos = leer_linea_csv(archivo, ',')) != NULL){
+		Carta carta;
+		carta.id = atoi(campos[0]);
+		strcpy(carta.valor, campos[1]);
+		strcpy(carta.color, campos[2]);
+
+		baraja->cartas[i] = carta;
+		i++;
+	}
+	baraja->num_cartas = i;
+	fclose(archivo); // Cierra el archivo después de leer todas las líneas
+}
+
+void mostrarCartas(Baraja *baraja){
+	for (int i = 0; i < baraja->num_cartas; i++){
+		Carta *carta = &baraja->cartas[i];
+		printf("%s de %s\n", carta->valor, carta->color);
+	}
+
+}
 
 
 
+int main(){
+	Baraja baraja;
+	leerCartas(&baraja);
 
-int main() {
-    // Estado inicial del puzzle
+	char opcion;
+	do{
+		puts("========================================");
+		puts("     		Poker");
+		puts("========================================");
 
-    char opcion;
-    do {
-        printf("\n***** EJEMPLO MENU ******\n");
-        puts("========================================");
-        puts("     Escoge método de búsqueda");
-        puts("========================================");
-        
-        puts("1) Búsqueda en Profundidad");
-        puts("2) Buscar en Anchura");
-        puts("3) Buscar Mejor Primero");
-        puts("4) Salir");
-    
-        printf("Ingrese su opción: ");
-        scanf(" %c", &opcion);
-    
-        switch (opcion) {
-        case '1':
-          //dfs(estado_inicial);
-          break;
-        case '2':
-          //bfs(estado_inicial);
-          break;
-        case '3':
-          //best_first(estado_inicial);
-          break;
-        }
-        presioneTeclaParaContinuar();
-        limpiarPantalla();
+		puts("1) cargar Cartas");
+		puts("2) Buscar en Anchura");
+		puts("3) Buscar Mejor Primero");
+		puts("4) Salir");
 
-  } while (opcion != '4');
+		printf("Ingrese su opción: ");
+		scanf(" %c", &opcion);
 
-  return 0;
+		switch (opcion){
+		case '1':
+			mostrarCartas(&baraja);
+			break;
+		case '2':
+			// bfs(estado_inicial);
+			break;
+		case '3':
+			// best_first(estado_inicial);
+			break;
+		}
+		presioneTeclaParaContinuar();
+		limpiarPantalla();
+
+	} while (opcion != '4');
+
+	return 0;
 }
